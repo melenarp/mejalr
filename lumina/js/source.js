1ec3af14298a31ea0929b79b759c6cc11e8ff015
richenbachen <richenbachen@hotmail.com>
2017-07-06 17:13:24 -0500

    Change java test inside index.html, correct original test from source.js

--- lumina/js/source.js
+++ lumina/js/source.js
@@ -1,20 +1,12 @@
 $(document).ready(function(){
 	$("#send_btn").click(function(){
 		$.get('get_DBdata.php', function(data){console.log(data);
-			$("#result").append("<table id='data_table'></table>");
-			$.each(data, function(i, row) {
-				var newRow = "<tr>";
-				newRow += "<td>"+row.Id_Mazo+"</td>";
-				newRow += "<td>"+row.Nombre_Mazo+"</td>";
-				newRow += "<td>"+row.Fuente_Mazo+"</td>";
-				newRow += "<td>"+row.Nombre_Anverso_Tarjeta+"</td>";
-				newRow += "<td>"+row.Nombre_Reverso_Tarjeta+"</td>";
-				newRow += "<td>"+row.Fecha_Hora_Crea_Mazo+"</td>";
-				newRow += "<td>"+row.Usuario_Crea_Mazo+"</td>";
-				newRow += "<td>"+row.Fecha_Hora_Modi_Mazo+"</td>";
-				newRow += "<td>"+row.Usuario_Modi_Mazo+"</td>";
-				newRow += "</tr>";
-				$("#data_table").append(newRow);
+			$.each(data, function(i, row) {				
+				//Add option Mazo to slect
+				var o = new Option(row.Nombre_Mazo, row.Nombre_Mazo);
+				//jquerify the DOM object 'o' so we can use the html method
+				$(o).html(row.Nombre_Mazo);
+				$("#sel1").append(o);
 			});
 		},
 		'json'
